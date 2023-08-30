88dcb20b7eb3db8da9c23bccf7ad08e0852bc419
Hossam Samir <hossamsamir@outlook.com>
2023-08-30 21:10:43 +0300

    playground

---
+++ playground.cpp
@@ -0,0 +1,10 @@
+// Created by Hossam Samir on 30/08/2023.
+
+#include <iostream>
+#include "Vector3D.h"
+
+int main()
+{
+    Vector3D v(1.0, 1.f, 1.f);
+    std::cout<< magnitude(v) <<std::endl;
+}
