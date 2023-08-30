e11396cd9d8f6cb70e77b20632205ab0d692c83f
Hossam Samir <hossamsamir@outlook.com>
2023-08-30 21:11:00 +0300

    minus operator

--- Vector3D.h
+++ Vector3D.h
@@ -1,5 +1,6 @@
 // Created by Hossam Samir on 30/08/2023.
 #pragma once
+#include <cmath>
 
 struct Vector3D {
     float x, y, z;
@@ -36,11 +37,23 @@ struct Vector3D {
     }
 };
 
-inline Vector3D operator*(const Vector3D &v, float s) {
+inline
+Vector3D operator*(const Vector3D &v, float s) {
     return (Vector3D(v.x * s, v.y * s, v.z * s));
 }
 
-inline Vector3D operator/(const Vector3D &v, float s) {
+inline
+Vector3D operator/(const Vector3D &v, float s) {
     s = 1.0f / s;
     return (Vector3D(v.x * s, v.y * s, v.z * s));
+}
+
+inline
+Vector3D operator-(const Vector3D &v) {
+    return Vector3D(-v.x, -v.y, -v.z);
+}
+
+inline float
+magnitude(Vector3D &v) {
+    return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
